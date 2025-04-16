e5a93c87ca8e879783920480ed557f8c0821d8cf
SidR <68519263+SidCoderMan@users.noreply.github.com>
2025-04-15 19:22:29 -0500

    Refactored API display

--- elite-react-project/src/App.jsx
+++ elite-react-project/src/App.jsx
@@ -1,18 +1,65 @@
 import {useState, useEffect} from "react";
 import Header from "./Header.jsx";
 import Button from "./Button.jsx";
+import "./App.css";
+const yelpKey = "http://localhost:3001/api/yelp?term=food&location=austin";
+const tmdbKey = "https://api.themoviedb.org/3/movie/popular?api_key=9369ac19724981e385d66aab69d57b62";
+
+
 function App() {
-  const {render, movie, food} = Button();
+    const [movie, setMovie] = useState("No movie");
+    const [food, setFood] = useState("No food");
+
+    async function getFood(){
+    try {
+        
+        const result = await fetch(yelpKey);
+        await result.json().then(json =>{
+            const randomIndex = Math.floor(Math.random() * 20);
+            setFood(json.businesses[randomIndex].name);
+            console.log(json.businesses[randomIndex].name);
+            return  json.businesses[randomIndex].name;
+        })
+      } catch (error) {
+        console.error(error);
+      }
+    }
   
+  async function getMovie(){
+    try {
+        
+        const result = await fetch(tmdbKey);
+        await result.json().then(json =>{
+            const randomIndex = Math.floor(Math.random() * 20);
+            setMovie(json.results[randomIndex].original_title);
+            console.log(json.results[randomIndex].original_title);
+            return   json.results[randomIndex].original_title;
+        })
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    function handleClick(){
+      getMovie();
+      getFood();
+    }
+    useEffect(() =>{
+      console.log({food, movie});
+    }, [food, movie])
  
   
   return(
-  <div id="app">
+  <div id="main">
     
     <Header />
-    <p>Movie: {movie}  Food: {food}</p>
-    {render}
-    
+    <div className = "container">
+      <div className = "content">
+        <p className = "text"> Restaurant: {food}</p>
+        <p className = "text"> Movie: {movie}</p>
+      </div>
+    <Button handleClick = {handleClick}></Button>
+      
+    </div>
   </div>
   );
 }
--- elite-react-project/src/Button.jsx
+++ elite-react-project/src/Button.jsx
@@ -1,60 +1,14 @@
 import {useState, useEffect} from "react";
+import "./App.css";
 
-const yelpKey = "http://localhost:3001/api/yelp?term=food&location=austin";
-const tmdbKey = "https://api.themoviedb.org/3/movie/popular?api_key=9369ac19724981e385d66aab69d57b62";
-
-function Button(){
-    const [movie, setMovie] = useState("");
-    const [food, setFood] = useState("");
+function Button({handleClick}){
     
-    //const[place, setPlace] = useState(0);
-    async function handleClick(){
-        await yelpGather();
-        await tmdbGather();
-    }
-    async function yelpGather(){
-        try {
-            
-            const result = await fetch(yelpKey);
-            await result.json().then(json =>{
-                const randomIndex = Math.floor(Math.random() * 20);
-                setFood(json.businesses[randomIndex].name);
-                return ""+ json.businesses[randomIndex].name
-            })
-          } catch (error) {
-            console.error(error);
-          }
-        }
     
-    async function tmdbGather(){
-        try {
-            
-            const result = await fetch(tmdbKey);
-            await result.json().then(json =>{
-                const randomIndex = Math.floor(Math.random() * 20);
-                setMovie(json.results[randomIndex].original_title);
-                return  json.results[randomIndex].original_title;
-            })
-          } catch (error) {
-            console.error(error);
-          }
-        }
     
+    return(
+      <button className = "button" onClick = {handleClick}> Match! </button>
+    )
         
-        return{
-        movie,
-        food,
-        render:(
-        <>
-        <button onClick = {handleClick} className="button">
-            Match!
-        </button>
-       
-        </>
-          )
-
-
-        };
-}
+  }
 
 export default Button
